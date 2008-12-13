40e11ffbdd719b58be92a24bf50ab0b0ef74a9fb
AD7six <andydawson76@yahoo.co.uk>
2008-12-13 13:21:04 +0100

    adding markitup source either accidentally deleted or not added to the git repo

---
+++ webroot/js/markitup/sets/default/set.js
@@ -0,0 +1,34 @@
+// ----------------------------------------------------------------------------
+// markItUp!
+// ----------------------------------------------------------------------------
+// Copyright (C) 2007 Jay Salvat
+// http://markitup.jaysalvat.com/
+// ----------------------------------------------------------------------------
+// Html tags
+// http://en.wikipedia.org/wiki/html
+// ----------------------------------------------------------------------------
+// Basic set. Feel free to add more tags
+// ----------------------------------------------------------------------------
+mySettings = {	
+	nameSpace:       "html",
+	onShiftEnter:  	{keepDefault:false, replaceWith:'<br />\n'},
+	onCtrlEnter:  	{keepDefault:false, openWith:'\n<p>', closeWith:'</p>'},
+	onTab:    		{keepDefault:false, replaceWith:'    '},
+	markupSet:  [ 	
+		{name:'Bold', key:'B', openWith:'(!(<strong>|!|<b>)!)', closeWith:'(!(</strong>|!|</b>)!)' },
+		{name:'Italic', key:'I', openWith:'(!(<em>|!|<i>)!)', closeWith:'(!(</em>|!|</i>)!)'  },
+		{name:'Stroke through', key:'S', openWith:'<del>', closeWith:'</del>' },
+		{separator:'---------------' },
+		{name:'Paragraph', openWith:'<p(!( class="[![Class]!]")!)>', closeWith:'</p>'  },
+        {name:'Code', openWith:'\n<pre>\n', closeWith:'\n</pre>\n' },
+		{separator:'---------------' },
+        {name:'Ul', openWith:'<ul>\n', closeWith:'</ul>\n' },
+        {name:'Ol', openWith:'<ol>\n', closeWith:'</ol>\n' },
+        {name:'Li', openWith:'<li>', closeWith:'</li>' },
+		{separator:'---------------' },
+		{name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
+		{separator:'---------------' },
+		{name:'Clean', className:'clean', replaceWith:function(markitup) { return markitup.selection.replace(/<(.*?)>/g, "") } },		
+		//{name:'Preview', className:'preview',  call:'preview'}
+	]
+}
